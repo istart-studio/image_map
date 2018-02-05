f163a055014da0f241fb6e05596c1edd29b2ae8c
dongyan <dong-_-yan@163.com>
2018-02-05 12:07:27 +0800

    初始化

--- show_image_server/src/main/java/studio/istart/tile/controller/SourceController.java
+++ show_image_server/src/main/java/studio/istart/tile/controller/SourceController.java
@@ -1,17 +1,19 @@
 package studio.istart.tile.controller;
 
-import com.sun.imageio.plugins.common.ImageUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.URI;
+import java.nio.file.Paths;
 
 /**
  * @author DongYan
@@ -23,15 +25,50 @@ import java.io.OutputStream;
 @RequestMapping("/source")
 public class SourceController {
 
-    @RequestMapping(value = "/{zLevel}/{xy}", method = RequestMethod.GET)
-    public void request(@PathVariable String zLevel, @PathVariable String xy,
-                        HttpServletResponse response) throws IOException {
+    @RequestMapping(value = "/tiles/{zLevel}/{xy}", method = RequestMethod.GET)
+    public void tiles(@PathVariable String zLevel, @PathVariable String xy,
+                      HttpServletResponse response) throws IOException {
         String dir = "/Users/dongyan/Downloads/tile/tiles/tiles_files/";
 
         //将图片输出给浏览器
-//        BufferedImage image = (BufferedImage) objs[1];
-//        response.setContentType("image/png");
-//        OutputStream os = response.getOutputStream();
-//        ImageIO.write(image, "png", os);
+        URI fileUri = Paths.get(dir, zLevel, xy + ".png").toUri();
+        log.info("img uri:{}", fileUri);
+        File file = new File(fileUri);
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            byte[] data = new byte[(int) file.length()];
+            inputStream.read(data);
+            inputStream.close();
+
+            response.setContentType("image/png");
+
+            OutputStream stream = response.getOutputStream();
+            stream.write(data);
+            stream.flush();
+            stream.close();
+        }
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public void image(@RequestParam String imageName, HttpServletResponse response) throws IOException {
+        String dir = "/Users/dongyan/Downloads/图片/";
+
+        //将图片输出给浏览器
+        URI fileUri = Paths.get(dir, imageName).toUri();
+        log.info("img uri:{}", fileUri);
+        File file = new File(fileUri);
+        try (FileInputStream inputStream = new FileInputStream(file)) {
+            byte[] data = new byte[(int) file.length()];
+            inputStream.read(data);
+            inputStream.close();
+
+            response.setContentType("image/jpg");
+
+            OutputStream stream = response.getOutputStream();
+            stream.write(data);
+            stream.flush();
+            stream.close();
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
